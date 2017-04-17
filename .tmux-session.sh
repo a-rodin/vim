b64c16f5c7b97a931b1772d6415be43f5b7c3f50
Alexander Rodin <rodin.alexander@gmail.com>
2017-04-17 10:02:11 +0300

    add tmux configuration

---
+++ .tmux-session.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SESSION_ID=$(tmux list-sessions -F "#{session_id}" | tail -n1)
+
+tmux rename-session -t $SESSION_ID _${SESSION_ID/\$/}
+SESSION_NAMES=$(tmux list-sessions -F "#{session_name}")
+SESSION_NUMBER=0
+while true; do
+  if grep "^$SESSION_NUMBER$" <<<"$SESSION_NAMES" >/dev/null ; then
+    ((SESSION_NUMBER++))
+  else
+    break
+  fi
+done
+tmux rename-session -t $SESSION_ID $SESSION_NUMBER
+
+HOST_ID=$(hostname | sum | cut -d' ' -f1)
+if [ -f ~/.tmux-host-colour ]; then
+  HOST_COLOUR=$(cat ~/.tmux-host-colour)
+else
+  HOST_COLOUR=4
+fi
+STYLE=colour$((1 + (2 * SESSION_NUMBER + HOST_COLOUR - 1) % 8))
+
+tmux set -t $SESSION_ID status-style bg=$STYLE
+tmux set -t $SESSION_ID pane-active-border-style bg=$STYLE
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 VUNDLE_PATH="$HOME/.vim/bundle/Vundle.vim"
 
 cd $(dirname $0)
-cp .vimrc ~/.vimrc
+cp .vimrc .tmux.conf .tmux-session.sh ~/
 cd "$VUNDLE_PATH" && git pull ||
     (rm -rf "$VUNDLE_PATH" && git clone https://github.com/VundleVim/Vundle.vim "$VUNDLE_PATH")
 vim -c PluginInstall -c 'silent VimProcInstall' -c qa
