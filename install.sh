453995dde085ee4cd175b7d6775f7f8b6380fbe3
Alexander Rodin <rodin.alexander@gmail.com>
2019-06-21 20:55:34 +0300

    Create symlinks instead of copying

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 VUNDLE_PATH="$HOME/.vim/bundle/Vundle.vim"
 
 cd $(dirname $0)
-cp .vimrc .tmux.conf .tmux-session.sh ~/
+rm -f ~/.vimrc ~/.tmux.conf
+ln -s $PWD/.vimrc ~/.vimrc
+ln -s $PWD/.tmux.conf ~/.tmux.conf
 cd "$VUNDLE_PATH" && git pull ||
     (rm -rf "$VUNDLE_PATH" && git clone https://github.com/VundleVim/Vundle.vim "$VUNDLE_PATH")
 vim -c PluginInstall -c 'silent VimProcInstall' -c qa
