fb62d07479d7c08ae7aa2b6a17a8d1822913c6ea
Alexander Rodin <rodin.alexander@gmail.com>
2019-06-30 20:39:28 +0300

    Set up TPM

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ cd $(dirname $0)
 rm -f ~/.vimrc ~/.tmux.conf
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+
 cd "$VUNDLE_PATH" && git pull ||
     (rm -rf "$VUNDLE_PATH" && git clone https://github.com/VundleVim/Vundle.vim "$VUNDLE_PATH")
 vim -c PluginInstall -c 'silent VimProcInstall' -c qa
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/tpm && ~/.tmux/plugins/tpm/scripts/install_plugins.sh
